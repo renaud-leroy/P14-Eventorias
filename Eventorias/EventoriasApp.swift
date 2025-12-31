3e571205d61a9fb672ce1409d5a477ed48fae207
cantoch <cantoch@icloud.com>
2025-12-31 02:18:07 +0100

    Démarrage du projet

---
+++ Eventorias/EventoriasApp.swift
@@ -0,0 +1,30 @@
+//
+//  EventoriasApp.swift
+//  Eventorias
+//
+//  Created by Renaud Leroy on 31/12/2025.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct EventoriasApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
